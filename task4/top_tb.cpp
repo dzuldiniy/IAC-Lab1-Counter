304168402a773d3b2db9345d3003e2e8ecebf600
Dzuldiniy Hussain <dzuldiniy.h@gmail.com>
2025-10-20 14:23:12 +0100

    completed task 4

---
+++ task4/top_tb.cpp
@@ -0,0 +1,54 @@
+#include "Vtop.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env) {
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+    //init top verilog instance
+    Vtop* top = new Vtop;
+    //init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp, 99);
+    tfp->open ("top.vcd");
+
+    //init Vbuddy
+    if (vbdOpen()!=1) return(-1);
+    vbdHeader("Lab 1: Task 4");
+    vbdSetMode(1); //one shot mode
+
+    //inititalize simulation inputs
+    top->clk = 1;
+    top->rst = 1;
+    top->v = 0;
+
+    //run simulation for many clock cycles
+    for (i=0; i<100; i++) {
+
+        //dump variables into VCD file and toggle clock
+        for (clk=0; clk<2; clk++) {
+            tfp->dump (2*i+clk);
+            top->clk = !top->clk;
+            top->eval();
+        }
+
+        //Show counter value on 7-segment display
+        vbdHex(3, (int(top->bcd) >> 8) & 0xF);
+        vbdHex(2, (int(top->bcd) >> 4)& 0xF);
+        vbdHex(1, (int(top->bcd)) & 0xF);
+        vbdCycle(i+1);
+        //end of Vbuddy output
+
+        top->rst = (i<2);
+        top->ld = vbdFlag(); //set ld to flag value
+        top->v = vbdValue(); //assign rotary encoder value
+        if (Verilated::gotFinish()) exit(0);
+    }
+    vbdClose();
+    tfp->close();
+    exit(0);
+}
